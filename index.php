d8a4aea5b642993dfb211e4fe7fdb60b3b960e03
vagner-silva7 <vagner.silva7@hotmail.com>
2022-10-24 23:37:33 -0300

    24102022

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-session_start();
+    
+    session_start();
+
+    if(!isset($_SESSION['carrinho'])){
+        $_SESSION['carrinho'] = array();
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +16,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja virtual - PHP</title>
 
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estilos.css">
 
 </head>
 <body>
@@ -27,7 +33,10 @@ session_start();
 
             <?php
 
-            $itens = array(['nome'=> 'curso 1','imagens'=>'item1.jpg','preco'=>'200'],['nome'=> 'curso 2','imagens'=>'item2.jpg','preco'=>'100'],['nome'=> 'curso 3','imagens'=>'item3.png','preco'=>'400']);
+            $itens = array( ['nome'=> 'curso 1','imagens'=>'imagens/item1.jpg','preco'=>'200'],
+                            ['nome'=> 'curso 2','imagens'=>'imagens/item2.jpg','preco'=>'100'],
+                            ['nome'=> 'curso 3','imagens'=>'imagens/item3.png','preco'=>'400']
+                        );
                         
             foreach ($itens as $key => $value){
            
@@ -35,8 +44,13 @@ session_start();
 
                 <div class="produto">
 
-                    <img src="<?php echo $value['imagens']?>" alt="">
+                    <img src="<?php echo $value['imagens']?>" alt=""/>
+
+                    <p><b><?php echo $value['nome']?> - R$ <?php echo $value['preco']?>,00</b></p>
+
                     <a href="?adicionar=<?php echo $key ?>">Adicionar ao carrinho</a>
+
+                    <a href="?remover=<?php echo $key ?>">Remover do carrinho</a>
 
                 </div>   
 
@@ -50,9 +64,29 @@ session_start();
             
             <?php
 
+            if(isset($_GET['remover'])){
+                /* adicionar ao carrinho */
+                $idProduto = (int) $_GET['remover'];
+                if(isset($itens[$idProduto])){
+                    /* echo 'o produto existe'; */
+                    if(isset($_SESSION['carrinho'][$idProduto])){
+                        $_SESSION['carrinho'][$idProduto]['quantidade']--;
+                    }else{
+                        $_SESSION['carrinho'][$idProduto] = array('quantidade'=>1,'nome'=> $itens[$idProduto]['nome'],'preco'=> $itens[$idProduto]['preco']);
+                    }
+                    echo "<script> alert('O item foi removido ao carrinho.'); </script>";
+                }else{
+                    echo "<script> alert('Você não poode remover este item ao carrinho - produto não existe.'); </script>";
+                }
+            } 
+
+            ?>
+
+            <?php
+
             if(isset($_GET['adicionar'])){
                 /* adicionar ao carrinho */
-                $idProduto = (int)$_GET['adicionar'];
+                $idProduto = (int) $_GET['adicionar'];
                 if(isset($itens[$idProduto])){
                     /* echo 'o produto existe'; */
                     if(isset($_SESSION['carrinho'][$idProduto])){
@@ -74,6 +108,8 @@ session_start();
 
         </div>
 
+           
+
             <?php
 
                 foreach ($_SESSION['carrinho'] as $key => $value){
@@ -85,7 +121,8 @@ session_start();
                     echo '<p> Nome: '.$value['nome'].'  |   Quantidade: '.$value['quantidade'].'  |   Preco: '.$value['preco']. '  |   Total: '.$value['quantidade']*$value['preco']; '</p>';
 
                     echo '</div>';
- 
+
+
                 }
           
             ?>
